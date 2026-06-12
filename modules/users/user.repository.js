a6bc65495b660228eb6d9467a57acd81ef247bd7
Sidhant Pandey <sidhantpandeynp6@gmail.com>
2026-06-12 22:29:47 +0530

    feature/backend-new-services: * Create API for update user (still in progress)

--- modules/users/user.repository.js
+++ modules/users/user.repository.js
@@ -2,8 +2,16 @@ const db = require("../../config/db");
 
 exports.findById = async (id) => {
   const user = await db.query(
-    "SELECT id,name,email,balance FROM users WHERE id=$1",
+    "SELECT id,name,email,balance,avatar FROM users WHERE id=$1",
     [id],
+  );
+  return user.rows[0];
+};
+
+exports.update = async (id, data) => {
+  const user = await db.query(
+    "UPDATE users SET name=$1, email=$2, avatar=COALESCE($3, avatar) WHERE id=$4 RETURNING id,name,email,balance,avatar",
+    [data.name, data.email, data.avatar, id],
   );
   return user.rows[0];
 };
--- modules/users/user.service.js
+++ modules/users/user.service.js
@@ -3,3 +3,7 @@ const repo = require("./user.repository");
 exports.getUser = async (id) => {
   return repo.findById(id);
 };
+
+exports.updateUser = async (id, data) => {
+  return repo.update(id, data);
+};
